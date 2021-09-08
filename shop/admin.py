3fe96d7e3dfbf665364b18a6438d2054e6eab17b
Ali Tavakoli <tavakoliali2001@gmail.com>
2021-09-08 16:35:26 +0430

    create models views urls admin and etc...

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.db import models
 
-# Register your models here.
+from .models import Product, Category
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    #prepopulated_fields = {'slug':('name',)}
+    def get_prepopulated_fields(self, request, obj=None):
+        return {'slug':('name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'updated']
+    list_filter = ['created', 'updated']
+
+    #prepopulated_fields = {'slug':('name',)}
+    
+    def get_prepopulated_fields(self, request, obj=None):
+        return {'slug':('name',)}
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
+from .models import Category , Product
 
-# Create your views here.
+
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.all()
+
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+    context = {
+            'categories':categories,
+            "category":category,
+            "products":products
+    }
+    return render(request, 'shop/list.html', context)
+
+
+
+def product_detail(request,id,slug):
+    product = get_object_or_404(Product, id=id , slug=slug)
+    context = {
+        'product':product
+    }
+    return render(request, "shop/detail.html", context)
